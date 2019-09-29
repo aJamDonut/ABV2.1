644c93981b126e6c7edba9fdb20ad96ee6558116
unknown <adamiandougherty@gmail.com>
2019-09-29 19:10:28 +0100

    All files

---
+++ js/languages/en.js
@@ -0,0 +1,308 @@
+_LANG = {
+	
+	GAME_NAME: "Arcade Builder",
+	
+	
+	/*LANGUAGE INFO START HERE*/
+	LANGUAGE: 'US-English',
+	/*LANGUAGE INFO END HERE*/
+	
+	HELLO: 'Hello',
+	
+	WELCOME_MESSAGE: "Welcome to Arcade Builder! The game is still EARLY ALPHA and may be unstable and cause low end machines to lag but it's fully playable! So have FUN! - JamDonut Games",
+	WELCOME_FOLLOWUP: "To get started I recommend buying a few pinball machines, then using the upgrade menu to progress from there!",
+	LINKBACKDESC: "Builder Community",
+	JOINCOMMUNITY: "Join our web community and share your creations with other people, get multiple saves, access to sandbox mode and more! - http://jamdonut.com",
+	MUST_UNLOCK_TOOL: "You must unlock this tool from your Upgrades first.",
+	MISSION_INCOMPLETE: 'It appears this mission is not complete!',
+	READY_TO_OPEN: 'Open the arcade? Guests will start arriving so make sure youre ready!',
+	READY_TO_CLOSE: 'Send all guests home and close the arcade?',
+	HOW_TO_UNLOCK: 'How to unlock: ',
+	OPEN: 'OPEN',
+	CLOSED: 'CLOSED',
+	START: 'Start',
+	/*ONE OFF UI WORDS ETC START HERE*/
+	MAIN_MENU: 'Arcade Builder 0.8 BETA',
+	ROOMS: 'Rooms',
+	ROOM: 'Room',
+	WAITING_FOR_DESCRIPTION: ' ',
+	NAME: 'Name',
+	STAFF: 'Staff',
+	BASICFURNI: 'Basic furniture',
+	ADVANCEDFURNI: 'Better furniture',
+	AGE: 'Age',
+	CLASS: 'Class',
+	TRAITS: 'Traits',
+	DESTROY: 'Dismantle Object',
+	ROTATE: 'Rotate',
+	TIME: 'Time',
+	CHALLENGES: 'Challenges',
+	FURNITURE: 'Furniture',
+	PINBALL: 'Pinball',
+	WALLS: 'Walls',
+	FLOORING: 'Flooring',
+	UPGRADES: 'Upgrades',
+	READTHOUGHTS: 'Read thoughts',
+	REDMACHINES: 'Red Machines',
+	BLUEMACHINES: 'Blue Machines',
+	TABLEPACK1: 'Table pack 1',
+	TABLEPACK2: 'Table pack 2',
+	COMPLETE: 'Complete',
+	CASH: 'Cash',
+	UPGRADE: 'Upgrade',
+	CONTINUE: 'Continue',
+	NEW_GAME: 'New game',
+	ABOUT: 'About',
+	YES: 'Yes',
+	NO: 'No',
+	TASK_COMPLETE: 'Task complete!',
+	OKAY: 'Okay',
+	CASH_REWARD: 'Cash reward: ',
+	KEEP_PLAYING: 'Keep playing',
+	MISSIONS: 'Tasks',
+	ZOOMIN: 'Zoom in',
+	ZOOMOUT: 'Zoom out',
+	FULLSCREEN: 'Fullscreen',
+	SAVE: 'Save',
+	MUTE: 'Mute',
+	LEAVERSHAPPY: 'Happy customers',
+	LEAVERSUNHAPPY: 'Unhappy customers',
+	UNMUTE: 'Unmute',
+	UNLOCKED: 'Unlocked',
+	GAMES_MACHINES: 'Arcade Machines',
+	LARGE_GAMES: 'Large Furniture',
+	CANT_AFFORD: 'You cannot afford this yet.',
+	/*ONE OFF UI WORDS ETC END HERE*/
+	
+	BUDGET: 'Budget',
+	PERSONALITY: 'Personality',
+	EXPERIENCE: 'Experience',
+	/*TOOL NAMES*/
+	BUILDWALL: 'Build wall',
+	DESTROYWALL: 'Demolish wall',
+	TABLETENNIS: 'Table tennis',
+	TABLEFOOTBALL: 'Table soccer',
+	
+	
+	/*FURNI NAMES START HERE*/
+	CHAIR: 'Chair',
+	DOOR: 'Door',
+	BED: 'Bed',
+	COUCH: 'Couch',
+	SHOWER: 'Shower',
+	SMALL_TABLE: 'Small table',
+	FRIDGE: 'Refridgerator',
+	PRESERVINGBIN: 'Preserving Bin',
+	RECEPTION: 'Reception',
+	SIDE_TABLE: 'Side table',
+	TABLE: 'Table',
+	ARMCHAIR: 'Armchair',
+	LIGHT: 'Light',
+	KITCHENSINK: 'Kitchen sink',
+	DINING_TABLE: 'Dining table',
+	COMPUTERDESK: 'Computer desk',
+	TREE: 'Tree',
+	SERVINGTABLE: 'Serving table',
+	TOILET: 'Toilet',
+	COOKER: 'Cooker',
+	PREPTABLE: 'Prep table',
+	COFFEETABLE: 'Coffee table',
+	SNACKMACHINE: 'Snack machine',
+	SODAMACHINE: 'Soda machine',
+	PIXELLAMP: 'Pixel lamp',
+	REDNEONSTRIP: 'Red neon strip',
+	POOLTABLE: 'Pool table',
+	AIRHOCKEY: 'Airhockey',
+	JANITOR: 'Janitor',
+	STOVE: 'Cooking Stove',
+	MINE: 'Mine',
+
+	STORAGEBOX: 'Storage Box',
+	GAMELOG: 'Game log',
+	/*NATURE*/
+	TOMATO: 'Tomato',
+	ONION: 'Onion',
+	LETTUCE: 'Lettuce',
+	COW: 'Cow',
+	PIG: 'Pig',
+	RAT: 'Rat',
+	COMPOSTBIN: 'Compost Bin',
+	TROUGH: 'Feeding Trough',
+	
+	
+	
+	/*Dont translate*/
+	CHANGE_MACHINE: 'Change Machine',
+	SUNWARS: 'Sun Wars',
+	FIREBALL: 'Fireball',
+	TWINKLEZONE: 'Twinkle Zone',
+	TACMAN: 'Tacman',
+	MONKEYKONG: 'Monkey Kong',
+	PACBOY: 'Pacboy',
+	FROGGY: 'Froggy',
+	/*End don't translate*/
+	
+	/*FURNI NAMES END HERE*/
+	
+	/*FLOORING NAMES*/
+	GRASS: 'Grass',
+	CONCRETE: 'Concrete',
+	RED: 'Red',
+	BLUE: 'Blue',
+	TILED: 'Tiled',
+	WOOD: 'Wood',
+	GRAVEL: 'Gravel',
+	
+	
+	/*PLANNING TOOLS*/
+	NEW_ROOM: 'New Room',
+	
+	/*STAFF NAMES START HERE*/
+	WORKMAN: 'Workman',
+	RECEPTIONIST: 'Receptionist',
+	CHEF: 'Chef',
+	MAID: 'Maid',
+	BELLBOY: 'Bell boy',
+	/*STAFF NAMES END HERE*/
+	
+	
+	/*ITEM DESCRIPTION*/
+	
+	DESC_CHAIR: 'Oh look its a chair!',
+	DESC_DESTROY: 'Destroy furniture for a small refund',
+	
+	DESC_FIREBALL: 'Your friendly neighbourhood fireball',
+	DESC_SUNWARS: 'A favourite with sci-fi fans',
+	DESC_TWINKLEZONE: 'It is a dimension as vast as space',
+	DESC_SNACKMACHINE: 'Yum yums for guests',
+	DESC_SODAMACHINE: 'Drinks machine',
+	
+	
+	/*ITEM DESCRIPTION END HERE*/
+	
+	/*ITEM STATUSES*/
+	OPERATIONAL: 'Operational',
+	BROKEN: 'Broken',
+	
+	/* MISSIONS */
+	
+	M1: 'Build a change machine and a pinball machine',
+	M2: 'Take in the populace and brainwash them into complying with the rules.',
+	M3: 'Control who gets to do what, and when.',
+	M4: 'Place down rooms and items to help with the process.',
+	M5: 'Keep your government overseers happy by meeting your mandated goals.',
+	M6: '',
+	M7: '',
+	M8: '',
+	M9: '',
+	M10: '',
+	M11: '',
+	M12: '',
+	M13: '',
+	M14: '',
+	M15: '',
+	M16: '',
+	M17: '',
+	M18: '',
+	MAX_PEEPS: 'Max customer capacity',
+	//Explainers
+	EXP_PRICES: 'Click on a machine to set its price',
+	
+	/* MISSIONS */
+	
+	
+	/* UPGRADES NUMBERS DO NOT MEAN ANYTHING */
+	
+	U1: 'Furniture tools',
+	U2: 'Set machine prices',
+	U3: 'Floor tools',
+	U4: 'Arcade machines',
+	U5: 'Wall tools',
+	U6: 'Large furniture',
+	U7: '',
+	U8: '',
+	U9: '',
+	U10: '',
+	
+	/* UPGRADES */
+	
+	/* CHALLENGES NUMBERS DO NOT MEAN ANYTHING  */
+	C1: 'Tutorial: Learn how to play the game!',
+	C2: 'Normal mode, start from scratch!',
+	C3: 'Easier mode, if you struggle in normal or tutorial!',
+	C4: 'Sandbox mode',
+	C5: 'Nightmare mode',
+	C6: '',
+	C7: '',
+	C8: '',
+	C9: '',
+	C10: '',
+	WINTUT: 'You have completed the challenge and can now move on to other challenges!',
+	
+	C1_DESC: 'Get used to the game',
+	C2_DESC: 'Play the full game on its intended difficulty',
+	C3_DESC: 'Play the full game starting with more cash',
+	C4_DESC: 'Start with loads of cash. No challenge at all',
+	C5_DESC: 'An open arcade with bad problems!',
+	C6_DESC: '',
+	C7_DESC: '',
+	C8_DESC: '',
+	C9_DESC: '',
+	C10_DESC: '',
+	
+	C1_REQ: '',
+	C2_REQ: 'Complete Tutorial',
+	C3_REQ: 'Complete Challenge 1',
+	C4_REQ: 'Complete Tutorial',
+	C5_REQ: '',
+	C6_REQ: '',
+	C7_REQ: '',
+	C8_REQ: '',
+	C9_REQ: '',
+	C10_REQ: '',
+	
+	
+	/* CHALLENGES */
+	
+	/* TUTORIAL MESSAGES  NUMBERS DO NOT MEAN ANYTHING */
+
+	TUT1: 'Welcome to Arcade Builder! Your arcade is shut right now but we will open it after adding furniture!',
+	TUT2: 'You can move the camera by holding RIGHT CLICK',
+	TUT3: 'Lets start by building a change machine. Click here to open up the machine building menu',
+	TUT4: 'Click the icon here to select the change machine',
+	TUT5: 'Place the furniture anywhere inside your arcade, if you need to move the camera you can hold RIGHT CLICK. You can also ROTATE using R',
+	TUT6: 'Change machines are used by guests before using your arcade machines. Next we should build a pinball machine',
+	TUT7: 'Lets open the machine building menu again',
+	TUT8: 'Now, pick the Sun Wars pinball machine',
+	TUT9: 'Place the pinball machine anywhere inside your arcade.',
+	TUT10: 'Great work! Pinball machines attract more customers but do not earn much money',
+	TUT11: 'You will often have more tasks to complete. You can find them by clicking here',
+	TUT12: 'Since you already placed a change machine, you can complete the first task! Click here to hand it in for a reward',
+	TUT13: 'Now we can open the Arcade and get some customers. Click here to close this menu',
+	TUT14: 'Click the arcade sign to open your arcade',
+	TUT16: 'Well done! Now that the arcade is open you will start to make money. Remember to check your tasks list and finish all the tasks!',
+	TUT17: 'You can unlock upgrades you need here! Customers will now start arriving. Good luck!',
+	
+	TOILETS: 'Toilets',
+	
+	SWITCH_STAGES: 'Are you sure you wish to change stages? Your current arcade will be closed',
+	PINBALL_MACHINES: 'Pinball Machines',
+	ARCADE_MACHINES: 'Arcade Machines',
+	CHALLENGE_COMPLETE: 'Challenge complete!',
+	MISSION_DYNAMIC_1: 'Have a minimum of ',
+	MISSION_DYNAMIC_2: ' built for your guests',
+	
+	
+	ELEC_BILL_1: 'Its time to pay the electricity bill!',
+	ELEC_BILL_2: ' Total Units: ',
+	ELEC_BILL_3: 'Cost per unit',
+	ELEC_BILL_4: 'Total Units:',
+	ELEC_BILL_5: 'Total Bill:',
+	ELEC_BILL_6: 'The amount has now been deducted from your balance',
+	ELEC_BILL_7: 'YOU CANNOT AFFORD THIS BILL. IF YOU MISS ANOTHER BILL YOU WILL BE BANKRUPT',
+	
+	
+	//{DYNAMIC_ADDITION}
+	
+};(function(){game.langLoaded()})();
+
